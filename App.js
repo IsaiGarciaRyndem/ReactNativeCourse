6a30ebab7a837e9034bcc4135668640efe21e852
Isai Amaury Garcia Flores <isai.garcia@ryndem.mx>
2024-09-18 18:20:26 -0600

    update: sqlite

--- App.js
+++ App.js
@@ -4,12 +4,26 @@ import {NavigationContainer} from "@react-navigation/native";
 import IconButton from "./components/UI/IconButton";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
-import { Colors } from './constants/colors';
+import {Colors} from './constants/colors';
 import Map from './screens/Map';
+import {useEffect, useState} from "react";
+import {init} from "./util/database";
 
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        init()
+            .then(() => {
+                setAppIsReady(true)
+        })
+            .catch(error => {
+            console.log(error)
+        });
+    },[]);
+
   return (
       <>
         <StatusBar style="dark" />
--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,5 +1,4 @@
 import { Pressable, StyleSheet, Text } from 'react-native';
-
 import { Colors } from '../../constants/colors';
 
 function Button({ onPress, children }) {
