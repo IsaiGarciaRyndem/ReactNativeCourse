0414aaf6c11e90aed9d9e008e2f1219de04edf0f
Isai Amaury Garcia Flores <isai.garcia@ryndem.mx>
2024-09-20 13:00:48 -0600

    update: configurar notificaciones

--- App.js
+++ App.js
@@ -1,10 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, Platform, StyleSheet, Text, View} from 'react-native';
+import * as Notifications from 'expo-notifications'
+import {useEffect} from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    };
+  }
+});
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to show notifications has not been granted.");
+      }
+/*      const pushTokenData = await  Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+
+      if(Platform.OS === 'android'){
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT
+        })
+      }*/
+    })();
+  }, []);
+
+  useEffect(() => {
+   const subscription1 =  Notifications.addNotificationReceivedListener((notification) => {
+      console.log('NOTIFICATION RECIVED');
+      console.log(notification);
+      const userName = notification.request.content.data.userName;
+      console.log(userName)
+    });
+
+   const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+     console.log('NOTIFICATION RESPONSE RECIVED');
+     console.log(response);
+     const userName = response.notification.request.content.data.userName;
+     console.log(userName)
+   });
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { userName: 'Max' }
+      },
+      trigger: { seconds: 2 },
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +84,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
